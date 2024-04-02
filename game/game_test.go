59c80ae8253ddfb251fa51665a2ad174271e496e
Nick Fang <fang.nicholas@gmail.com>
2024-04-02 11:55:32 -0500

    creating test for DeepCopy function.

--- game/game_test.go
+++ game/game_test.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,9 +15,47 @@ func TestNewGame(t *testing.T) {
 
 func TestNextDeckCard(t * testing.T) {
 	g := NewGame()
+
 	g.Cards = g.Cards[:0]
 	err := g.NextDeckCard()
 	if err.Error() != "no more cards in the deck" {
 		t.Error("Expected error message not shown.")
+	}
+
+}
+
+func TestDeepCopy(t *testing.T) {
+	game := NewGame()
+	gameShallow := game
+	gameDeep := game.DeepCopy()
+
+	// Checking each part of the game Cards, Board, Stacks and CurrentCardIndex
+	game.MoveFromDeckToStacks()
+	if !game.IsEqual(gameShallow) {
+		fmt.Printf("Shallow copy should be updated when original is updated")
+	}
+	if game.IsEqual(gameDeep) {
+		t.Error("Deep copy should not be updated when original is updated.")
+	}
+	game.Cards.RandomShuffle()
+	if !game.IsEqual(gameShallow) {
+		fmt.Printf("Shallow copy should be updated when original is updated")
+	}
+	if game.IsEqual(gameDeep) {
+		t.Error("Deep copy should not be updated when original is updated.")
+	}
+	game.DealBoard()
+	if !game.IsEqual(gameShallow) {
+		fmt.Printf("Shallow copy should be updated when original is updated")
+	}
+	if game.IsEqual(gameDeep) {
+		t.Error("Deep copy should not be updated when original is updated.")
+	}
+	game.NextDeckCard()
+	if !game.IsEqual(gameShallow) {
+		fmt.Printf("Shallow copy should be updated when original is updated")
+	}
+	if game.IsEqual(gameDeep) {
+		t.Error("Deep copy should not be updated when original is updated.")
 	}
 }
